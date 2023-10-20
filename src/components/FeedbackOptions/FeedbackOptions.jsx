4efb388d1e00a60360aea4731bdf48215aa5c55b
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-10-20 18:02:33 +0200

    bugfix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,17 +1,15 @@
-import { Button, Wrapper } from "./FeedbackOptions.module";
-
+import { Button, Wrapper } from './FeedbackOptions.module';
 
 export const FeedbackOptions = ({ options, handleFeedback }) => {
-    return (
-        <>
-            <Wrapper>
-                {options.map(option => (
-                    <Button  key={option} 
-                    onClick={() => handleFeedback(option)} >
-                        {option}
-                    </Button>
-                ))}
-            </Wrapper>
-        </>
-    );
+  return (
+    <>
+      <Wrapper>
+        {options.map(option => (
+          <Button key={option} onClick={() => handleFeedback(option)}>
+            {option}
+          </Button>
+        ))}
+      </Wrapper>
+    </>
+  );
 };
